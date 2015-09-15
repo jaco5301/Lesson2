2184be32fa017634c7e2718790cb8bb57d6ceeaf
jaco5301 <jaco5301@ISS147D8-25.scdsb.pri>
2015-09-15 10:35:26 -0400

    stuff

--- src/BarGraph.java
+++ src/BarGraph.java
@@ -23,7 +23,44 @@ public class BarGraph {
         
         
         SketchPadWindow s = new SketchPadWindow(301, 250);
-        Pen p = new WiggleRainbowPen(s);
+        Pen p = new RainbowPen(s);
+        p.setWidth(30);
+        p.up();
+        p.move(20-150, 10-125);
+        p.down();
+        p.move(20-150, bar1-125);
+        p.up();
+        p.move(10-150, 200-125);
+        p.down();
+        p.drawString("" + bar1);
+       
+        p.up();
+        p.move(70-150, 10-125);
+        p.down();
+        p.move(70-150, bar2-125);
+        p.up();
+        p.move(60-150, 200-125);
+        p.down();
+        p.drawString("" + bar2);
+        
+        p.up();
+        p.move(120-150, 10-125);
+        p.down();
+        p.move(120-150, bar3-125);
+        p.up();
+        p.move(110-150, 200-125);
+        p.down();
+        p.drawString("" + bar3);
+        
+        p.up();
+        p.move(170-150, 10-125);
+        p.down();
+        p.move(170-150, bar4-125);
+        p.up();
+        p.move(160-150, 200-125);
+        p.down();
+        p.drawString("" + bar4);
+        
         
     }
     
